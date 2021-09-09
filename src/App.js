671cb129955f6bf3466256ef9522b05bd6f8df88
Mathilda Pettersson <mathilda.pettersson@medieinstitutet.se>
2021-09-09 11:13:21 +0200

    heroku deploy

--- src/App.js
+++ src/App.js
@@ -98,6 +98,7 @@ function App() {
     useEffect(() => {
         getDaysArrayByMonth();
         getEmptyDays();
+        // eslint-disabke.next-line react-hooks/exhaustive-deps
     }, [monthInNr]);
 
     // - - - - -  - -  CHOOSED DAY - - - -  - - - //
@@ -125,7 +126,7 @@ function App() {
 
         setSavedTodo(saveTodo);
 
-        fetch('http://localhost:3001/new', {
+        fetch('https://calendar-mathildap.herokuapp.com/new', {
             method: 'post',
             headers: { 'Content-type': 'application/json' },
             body: JSON.stringify(saveTodo),
@@ -136,7 +137,7 @@ function App() {
 
     // GET TODOS FROM DB
     useEffect(() => {
-        fetch('http://localhost:3001/get')
+        fetch('https://calendar-mathildap.herokuapp.com/get')
             .then((res) => {
                 if (res.ok) {
                     return res.json();
@@ -149,7 +150,7 @@ function App() {
     // DELETE TODO
     const deleteTask = (id) => {
         let todoId = { id };
-        fetch('http://localhost:3001/delete', {
+        fetch('https://calendar-mathildap.herokuapp.com/delete', {
             method: 'post',
             headers: { 'Content-type': 'application/json' },
             body: JSON.stringify(todoId),
@@ -172,7 +173,7 @@ function App() {
 
         let todoId = { id: id.id, done: trueOrFalse };
 
-        fetch('http://localhost:3001/update', {
+        fetch('https://calendar-mathildap.herokuapp.com/update', {
             method: 'post',
             headers: { 'Content-type': 'application/json' },
             body: JSON.stringify(todoId),
